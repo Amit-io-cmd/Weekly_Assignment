4da1312520f02c101dc53ebbd0c3aa37053bbf14
Amit-io-cmd <amit857680@gmail.com>
2025-09-23 10:19:51 +0530

    add

---
+++ 19sep.py
@@ -0,0 +1,6 @@
+List = [1, 4, 5, 6, 7, 4, 3, 2, 1]
+new_list = []
+for i in List:
+    if i not in new_list:
+        new_list.append(i)  
+
---
+++ pattern.py
@@ -0,0 +1,10 @@
+# for i in range(5):
+#     for j in range(i):
+#         print(i,end="")
+#         i+= 1
+#     print()
+for i in range(4):
+    for j in range(4):      
+     print('*', end="") 
+    print()      
+
